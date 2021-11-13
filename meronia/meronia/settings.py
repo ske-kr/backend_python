c5a4b9004ba3c0daff50afec3990f6f6d4c57722
ske-kr <54828276+ske-kr@users.noreply.github.com>
2021-11-13 19:30:28 +0900

    Update settings.py

--- meronia/meronia/settings.py
+++ meronia/meronia/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
